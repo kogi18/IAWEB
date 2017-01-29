9964d9dadbdc585910bce84a6d267e51dbcbc39a
ROK KOGOVŠEK <rok.kogovsek@gmail.com>
2017-01-29 06:08:23 +0100

    Fixed HTML highlightining

--- Project/rSlidyAnimated/js/rslidy.js
+++ Project/rSlidyAnimated/js/rslidy.js
@@ -1632,7 +1632,7 @@ function openSweetAlertImage(string) {
 }
  
 
-
+// ---
 // Description: Enables the browser to first draw the loader, before initialization is started
 // ---
 function preload() {
@@ -1646,6 +1646,25 @@ function preload() {
 	*/
 }
 
+// ---
+// Description: HTML Code snipplets are rendered since we are in HTML - replace all tags with &lt; and &gt;
+// ---
+function correctCodeSnipplets(){
+	var preItems = document.getElementsByTagName('pre');
+	for (var p=0; p<preItems.length; p++) {
+		var codeItems = preItems[p].getElementsByTagName('code');
+		for (var c=0; c<codeItems.length; c++) {
+		    var item = codeItems[c];
+		    if (item.classList.contains("html") || item.classList.contains("xhtml") || item.classList.contains("xml")) {
+		    	var code = item.innerHTML;
+		    	code = code.split("<").join("&lt;");
+		    	code = code.split(">").join("&gt;");
+		    	item.innerHTML = code;
+		    }
+		}
+	}
+
+}
 
 function start() {
 	var start = performance.now();
@@ -1654,8 +1673,10 @@ function start() {
 	var rslidy = new Rslidy();
 	// timeout allows the repaint to catch a break between preload and init
 	setTimeout(function(){
+		correctCodeSnipplets();
 		rslidy.init();
 		// load the set markup languages of highlight.js
+		hljs.configure({	tabReplace: '   '});// 2 spaces instead of tabs
 		hljs.initHighlighting();
 	}, 1);
 }
